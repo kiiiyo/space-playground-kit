748d39eab272a899786fdaefb4041c8b91d5c7b2
takashi.kiyota <kiiiyo.jp@gmail.com>
2025-12-03 17:45:43 +0900

    chore: update Next.js configuration to enable standalone output and typed routes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  typedRoutes: true,
 };
 
 export default nextConfig;
